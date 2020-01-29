c2f5e9b131277a764500219d13f4816ca79de3cf
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2020-01-29 20:51:08 +0700

    Showed all task list on index page

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def index(request):
+    # get all task lists from database to show on index page.
+    task_list = Task.objects.all
+    context = {'task_list': task_list}
+    return render(request, 'lists/index.html', context)
