3867988622b5e8dbb65a71b64fb7c3932efe775a
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2020-01-29 21:16:41 +0700

    User can add new task

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import  HttpResponseRedirect
+from django.urls import reverse
 from .models import Task
 
 def index(request):
@@ -6,3 +8,13 @@ def index(request):
     task_list = Task.objects.all
     context = {'task_list': task_list}
     return render(request, 'lists/index.html', context)
+
+def AddNewTask(request):
+    # if text input isn't empty then save new one into database.
+    if request.POST.get('task'):
+        task = Task()
+        task.task_text = request.POST.get('task')
+        task.save()
+        # After saved successful, redisplay to update tasks.
+        return HttpResponseRedirect(reverse('index'))
+ 
