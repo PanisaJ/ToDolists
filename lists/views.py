0fceb63d94b8ec5c2c858c921dc3f8d7f1935294
PanisaJ <s5701012630140@email.kmutnb.ac.th>
2020-01-29 21:43:26 +0700

    User can delete task if completed

--- lists/views.py
+++ lists/views.py
@@ -17,4 +17,19 @@ def AddNewTask(request):
         task.save()
         # After saved successful, redisplay to update tasks.
         return HttpResponseRedirect(reverse('index'))
- 
+
+def DeleteTask(request):
+    try:
+        # get task is selected from radio input.
+        selected_task = Task.objects.get(pk=request.POST['Task'])
+    # if user didn't select and clicked 'Completed' button,
+    # error massage will appear to notice.
+    except (KeyError, Task.DoesNotExist):
+        return render(request, 'lists/index.html', {
+            'task_list': Task.objects.all,
+            'error_message': "You didn't select a task.",
+        })
+    else:
+        # user selected one, selected task is removed and reload page. 
+        selected_task.delete()
+        return HttpResponseRedirect(reverse('index'))
